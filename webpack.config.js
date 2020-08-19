b84b0954acf8cefe6a272f03328066fc79b57f5c
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-19 13:53:55 -0500

    Se agregaron las caracteristicas de webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize:false,
+                            attributes: false,
+                        },
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template:'./src/index.html',
+            filename: './index.html'
+        }),
+    ]
+
+
+
+
+}
+
+
+
