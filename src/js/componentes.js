c2afe416fbbed8e1bad5f60c184bafaa346b4f1c
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-19 14:13:01 -0500

    Se agregó el css y caracteriticas de css adicionales

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,3 +1,8 @@
+import '../css/componentes.css'
+
+
+
+
 export const saludar = (nombre) =>{
     console.log('Creando etiqueta h1');
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
     module: {
         rules: [
             {
+test: /\.css$/,
+use:[
+    'style-loader',
+    'css-loader'
+]
+            },
+            {
                 test: /\.html$/,
                 use: [
                     {
