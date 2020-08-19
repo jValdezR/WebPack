3d66262240e4b5466acbe48a8d452fd42a55adba
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-19 15:02:42 -0500

    Incompleto el agregar imagenes a serv dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { saludar } from './js/componentes.js'
-
-
-
+import './styles.css'
 
 const nombre = 'Dany';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,59 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = {
 
-    mode: 'development',
+    mode: 'production',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
     module: {
         rules: [
             {
-test: /\.css$/,
-use:[
-    'style-loader',
-    'css-loader'
-]
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
             },
             {
                 test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    minimize: false,
+                    attributes: false,
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     {
-                        loader: 'html-loader',
+                        loader: 'file-loader',
                         options: {
-                            minimize:false,
-                            attributes: false,
-                        },
+                            esModule: false
+                        }
                     }
                 ]
             }
         ]
     },
-    plugins:[
+    plugins: [
         new HtmlWebPackPlugin({
-            template:'./src/index.html',
+            template: './src/index.html',
             filename: './index.html'
         }),
+        new MiniCssExtractPlugin({
+            flename: '[name].css',
+            ignoreOrder: false
+        })
     ]
 
 
